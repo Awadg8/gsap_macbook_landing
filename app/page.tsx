bf22432ef84b6b0799a35afb4069e956842c1c76
Awadhesh Gupta <awadheshg798@gmail.com>
2025-12-15 14:18:30 +0530

    Add Hero component and update Next.js version
    Introduced a new Hero component with video and product info, and integrated it into the home page. Also updated Next.js dependency from 16.0.8 to ^16.0.10 in package.json.

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import Hero from "@/component/Hero";
 import NavBar from "@/component/NavBar";
 
 export default function Home() {
   return (
     <div>
       <NavBar />
+      <Hero />
     </div>
   );
 }
