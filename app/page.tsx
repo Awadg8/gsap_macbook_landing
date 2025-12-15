7497ef31c941ee360e50a410c8349e63270b29ef
Awadhesh Gupta <awadheshg798@gmail.com>
2025-12-15 15:28:26 +0530

    Add ProductViewer component with Zustand store
    Introduces a new ProductViewer component for selecting Macbook color and size, using Zustand for state management. Updates dependencies to include clsx and zustand, and integrates the ProductViewer into the home page.

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import Hero from "@/component/Hero";
 import NavBar from "@/component/NavBar";
+import ProductViewer from "@/component/ProductViewer";
 
 export default function Home() {
   return (
     <div>
       <NavBar />
       <Hero />
+      <ProductViewer />
     </div>
   );
 }
