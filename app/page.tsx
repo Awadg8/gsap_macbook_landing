0749ddc76ddcaedfd3bafe8f3efd8d0f18dd0a85
Awadhesh Gupta <awadheshg798@gmail.com>
2025-12-16 17:26:36 +0530

    Add Performance, Features, Highlight, and Footer components
    Introduced new components: Performance, Features, Highlight, and Footer. Updated the Home page to include these components, enhancing the structure and content of the main page.

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,12 @@ import Hero from "@/component/Hero";
 import NavBar from "@/component/NavBar";
 import ProductViewer from "@/component/ProductViewer";
 import Showcase from "@/component/Showcase";
+import Performance from "@/component/Performance";
 import gsap from "gsap";
 import { ScrollTrigger } from "gsap/all";
+import Features from "@/component/Features";
+import Highlight from "@/component/Highlight";
+import Footer from "@/component/Footer";
 
 gsap.registerPlugin(ScrollTrigger);
 
@@ -14,6 +18,10 @@ export default function Home() {
       <Hero />
       <ProductViewer />
       <Showcase />
+      <Performance />
+      <Features />
+      <Highlight />
+      <Footer />
     </main>
   );
 }
